43471ed518432181c9b8f9c52959de048fea5d17
AdrianaM2S <adriana.m.marrero16@gmail.com>
2025-04-20 21:11:11 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# List of temporary storage for users and products
+users = []
+products = []
+
+# Route to GET /info
+@app.route("/info", methods=["GET"])
+def info():
+    return jsonify({
+        "message": "Sistema de gestion de productos y usuarios"
+    })
+
+# Route to POST /create_user
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    data = request.json
+    # Data validation
+    name = data.get("name")
+    email = data.get("email")
+    Id = data.get("Id")
+
+    if not name or not email:
+        return jsonify({
+            "error": "Required fields are missing. Must include 'name' and 'email'."
+        }), 400
+
+    user = {"Id": Id, "name": name, "email": email}
+    users.append(user)
+    return jsonify({
+        "message": "User created", "users": users
+        })
+
+# Route to GET /users
+@app.route("/users", methods=["GET"])
+def list_users():
+    return jsonify({
+        "users": users
+    })
+
+# Route to POST /create_product
+@app.route("/create_product", methods=["POST"])
+def create_product():
+    data = request.json
+    productId = data.get("productId")
+    name = data.get("name")
+    price = data.get("price")
+    available = data.get("available", True)
+
+    if not name or not price:
+        return jsonify({
+            "error": "Name and price are required"
+            }), 400
+
+    product = {"productId": productId, "name": name, "price": price, "available": available}
+    products.append(product)
+    return jsonify({
+        "message": "Product created", "product": product
+        })
+
+# Route to GET /products
+@app.route("/products", methods=["GET"])
+def list_products():
+    return jsonify({"products": products})
+
+if __name__ == "__main__":
+    app.run(debug=True)
